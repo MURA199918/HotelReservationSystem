e269976879b31b1bf516e74bf225b86a4333a961
MURA199918 <muralisaranr18@gmail.com>
2020-12-04 19:29:50 +0530

    Added UC1 to add Hotel details

--- src/main/java/HotelReservation.java
+++ src/main/java/HotelReservation.java
@@ -1,5 +1,17 @@
+import java.util.HashMap;
+
 public class HotelReservation {
+    static HashMap<String,Integer> Hotelweekdayregular = new HashMap<>();
+    static HashMap<String,Integer> Hotelweekendregular = new HashMap<>();
     public static void main(String[] args) {
         System.out.println("............Welcome to Hotel Reservation Program.............");
+        Hotelweekdayregular.put("LakeWood",110);
+        Hotelweekdayregular.put("BridgeWood",160);
+        Hotelweekdayregular.put("RidgeWood",220);
+        Hotelweekendregular.put("LakeWood",90);
+        Hotelweekendregular.put("BridgeWood",60);
+        Hotelweekendregular.put("RidgeWood",150);
+        System.out.println("Weekday details "+Hotelweekdayregular);
+        System.out.println("Weekend details "+Hotelweekendregular);
     }
 }
