e5f1067fea369cabb6264fd027e48991410fe632
MURA199918 <muralisaranr18@gmail.com>
2020-12-05 11:15:06 +0530

    Added UC2 to find cheap hotel for date range

--- src/main/java/HotelReservation.java
+++ src/main/java/HotelReservation.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class HotelReservation {
     public static HashMap<String,Integer> Hotelweekdayregular = new HashMap<>();
@@ -19,8 +20,57 @@ public class HotelReservation {
             System.out.println("Hotel name: "+entry.getKey()+" Rates: "+entry.getValue());
         }
     }
+    public static int dayofweek(String startdate, String enddate)
+    {
+        String[] startarr = startdate.toLowerCase().split(" ");
+        int date = Integer.parseInt(startarr[0]);
+        String month = (startarr[1]);
+        int m = 0;
+        int year = Integer.parseInt(startarr[2]);
+        if(month.equals("jan")){
+            m = 1;
+        }
+        else if(month.equals("feb")){
+            m = 2;
+        }
+        else if(month.equals("mar")){
+            m = 3;
+        }
+        else if(month.equals("apr")){
+            m = 4;
+        }
+        else if(month.equals("may")){
+            m = 5;
+        }
+        else if(month.equals("june")){
+            m = 6;
+        }
+        else if(month.equals("july")){
+            m = 7;
+        }
+        else if(month.equals("aug")){
+            m = 8;
+        }
+        else if(month.equals("sep")){
+            m = 9;
+        }
+        else if(month.equals("oct")){
+            m = 10;
+        }
+        else if(month.equals("nov")){
+            m = 11;
+        }
+        else if(month.equals("dec")){
+            m = 12;
+        }
+        int t[] = { 0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4 };
+        year -= (m < 3) ? 1 : 0;
+        return ( year + year/4 - year/100 + year/400 + t[m-1] + date) % 7;
+    }
     public static void main(String[] args) {
         System.out.println("............Welcome to Hotel Reservation Program.............");
+        Scanner sc = new Scanner(System.in);
+        HotelReservation hotelReservation = new HotelReservation();
         Hotelweekdayregular.put("LakeWood",110);
         Hotelweekdayregular.put("BridgeWood",160);
         Hotelweekdayregular.put("RidgeWood",220);
@@ -29,6 +79,10 @@ public class HotelReservation {
         Hotelweekendregular.put("RidgeWood",150);
         System.out.println("Weekday details "+Hotelweekdayregular);
         System.out.println("Weekend details "+Hotelweekendregular);
+        System.out.println("Enter date of the Customer");
+        int date = sc.nextInt();
+        System.out.println("Enter month of the Customer");
+        String month = sc.next();
     }
 
 
--- src/test/java/HotelReservationTest.java
+++ src/test/java/HotelReservationTest.java
@@ -24,4 +24,30 @@ public class HotelReservationTest {
         Assert.assertEquals(110,value);
     }
 
+    @Test
+    public void Test2ToCheckCheapHotelforGivenDate(){
+        HotelReservation hotelReservation = new HotelReservation();
+        hotelReservation.addweekdayRegular("Lakewood",110);
+        hotelReservation.addweekdayRegular("Bridgewood",160);
+        hotelReservation.addweekdayRegular("Ridgewood",220);
+        //hotelReservation.addweekendRegular("Lakewood",90);
+        //hotelReservation.addweekendRegular("Bridgewood",60);
+        //hotelReservation.addweekendRegular("Ridgewood",150);
+        hotelReservation.printdetails();
+        String cheapesthotel = null;
+        try{
+            int dayvalue = hotelReservation.dayofweek("16 Mar 2020","17 Mar 2020");
+            if(dayvalue>=1 && dayvalue<=5){
+                cheapesthotel = "Lakewood";
+            }
+            else{
+                cheapesthotel = "Bridgewood";
+            }
+        }
+        catch (Exception e){
+            System.out.println("Found exception");
+        }
+        Assert.assertEquals("Lakewood",cheapesthotel);
+    }
+
 }
