8e4abb68bd5bbdc8cd55d3719e75004c064004a4
MURA199918 <muralisaranr18@gmail.com>
2020-12-05 09:59:09 +0530

    Added UC1 to add hotel details

--- src/main/java/HotelReservation.java
+++ src/main/java/HotelReservation.java
@@ -9,6 +9,16 @@ public class HotelReservation {
     public static void addweekendRegular(String hotelname, int rate) {
         Hotelweekendregular.put(hotelname,rate);
     }
+    public static void printdetails(){
+        System.out.println("Weekday rates of hotel");
+        for (HashMap.Entry<String,Integer> entry : Hotelweekdayregular.entrySet()){
+               System.out.println("Hotel name: "+entry.getKey()+" Rates: "+entry.getValue());
+        }
+        System.out.println("Weekend rates of hotel");
+        for(HashMap.Entry<String,Integer> entry : Hotelweekendregular.entrySet()){
+            System.out.println("Hotel name: "+entry.getKey()+" Rates: "+entry.getValue());
+        }
+    }
     public static void main(String[] args) {
         System.out.println("............Welcome to Hotel Reservation Program.............");
         Hotelweekdayregular.put("LakeWood",110);
--- src/test/java/HotelReservationTest.java
+++ src/test/java/HotelReservationTest.java
@@ -1,15 +1,19 @@
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.Map;
 
 public class HotelReservationTest {
     @Test
     public void Test1toaddHoteldetails(){
         HotelReservation hotelReservation = new HotelReservation();
-        HotelReservation.addweekdayRegular("Lakewood",110);
-        HotelReservation.addweekdayRegular("Bridgewood",160);
-        HotelReservation.addweekdayRegular("Ridgewood",220);
-        HotelReservation.addweekendRegular("Lakewood",90);
-        HotelReservation.addweekendRegular("Bridgewood",60);
-        HotelReservation.addweekendRegular("Ridgewood",150);
+        hotelReservation.addweekdayRegular("Lakewood",110);
+        hotelReservation.addweekdayRegular("Bridgewood",160);
+        hotelReservation.addweekdayRegular("Ridgewood",220);
+        hotelReservation.addweekendRegular("Lakewood",90);
+        hotelReservation.addweekendRegular("Bridgewood",60);
+        hotelReservation.addweekendRegular("Ridgewood",150);
+        hotelReservation.printdetails();
     }
 
 }
