1dc3634de50603d08ecaafeeaa9fa3d5f8b60c07
MURA199918 <muralisaranr18@gmail.com>
2020-12-05 09:36:26 +0530

    Added UC1 to add hotel details

--- src/test/java/HotelReservationTest.java
+++ src/test/java/HotelReservationTest.java
@@ -1,5 +1,15 @@
 import org.junit.Test;
 
 public class HotelReservationTest {
+    @Test
+    public void Test1toaddHoteldetails(){
+        HotelReservation hotelReservation = new HotelReservation();
+        HotelReservation.addweekdayRegular("Lakewood",110);
+        HotelReservation.addweekdayRegular("Bridgewood",160);
+        HotelReservation.addweekdayRegular("Ridgewood",220);
+        HotelReservation.addweekendRegular("Lakewood",90);
+        HotelReservation.addweekendRegular("Bridgewood",60);
+        HotelReservation.addweekendRegular("Ridgewood",150);
+    }
 
 }
